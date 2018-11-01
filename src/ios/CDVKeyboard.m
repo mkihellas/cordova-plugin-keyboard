465c3a6cca294595d5f6e76cfddbc89877615bb1
mkihellas <35369394+mkihellas@users.noreply.github.com>
2018-11-01 11:02:28 +0200

    Update CDVKeyboard.m

--- src/ios/CDVKeyboard.m
+++ src/ios/CDVKeyboard.m
@@ -183,11 +183,11 @@ static IMP WKOriginalImp;
     screen = [self.webView convertRect:screen fromView:nil];
 
     // if the webview is below the status bar, offset and shrink its frame
-    if ([self settingForKey:@"StatusBarOverlaysWebView"] != nil && ![[self settingForKey:@"StatusBarOverlaysWebView"] boolValue]) {
+    //if ([self settingForKey:@"StatusBarOverlaysWebView"] != nil && ![[self settingForKey:@"StatusBarOverlaysWebView"] boolValue]) {
         CGRect full, remainder;
         CGRectDivide(screen, &remainder, &full, statusBar.size.height, CGRectMinYEdge);
         screen = full;
-    }
+    //}
 
     // Get the intersection of the keyboard and screen and move the webview above it
     // Note: we check for _shrinkView at this point instead of the beginning of the method to handle
